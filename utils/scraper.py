0c750fe01014889bbecf5ea78ad3b13cea045312
Alex Termure <124639246+jackjakarta@users.noreply.github.com>
2024-11-06 22:59:44 +0100

    structured outputs for classification (#1)

--- utils/scraper.py
+++ utils/scraper.py
@@ -11,26 +11,26 @@ def scrape(website_url):
 
     if response.status_code == 200:
         # Parse HTML content
-        soup = BeautifulSoup(response.text, 'html.parser')
-        img_tags = soup.find_all('img')
+        soup = BeautifulSoup(response.text, "html.parser")
+        img_tags = soup.find_all("img")
 
         images_path = config("IMAGES_FOLDER", default="images")
         os.makedirs(images_path, exist_ok=True)
 
         for img_tag in img_tags:
             # Get img URL
-            img_src = img_tag.get('src')
+            img_src = img_tag.get("src")
 
             # URL Validation
-            if img_src and not img_src.startswith('data:'):
+            if img_src and not img_src.startswith("data:"):
                 # URL Join
                 img_url = urljoin(website_url, img_src)
 
                 # Image content
                 img_data = requests.get(img_url).content
-                img_filename = os.path.join('images', os.path.basename(img_url))
+                img_filename = os.path.join("images", os.path.basename(img_url))
 
-                with open(img_filename, 'wb') as img_file:
+                with open(img_filename, "wb") as img_file:
                     img_file.write(img_data)
 
                 print(f"Downloaded: {img_filename}")
